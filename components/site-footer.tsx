8e7dfa44deeddfc4d12282f39d3ad50d1e0d7711
Mingfun Wong <mingfun.wong.chn@gmail.com>
2023-06-01 12:52:13 +0000

    refine ui

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,6 +1,6 @@
 export function SiteFooter() {
   return (
-    <footer className="border-t py-6 md:py-0">
+    <footer className="py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,7 +4,7 @@ import { MobileNav } from "@/components/mobile-nav"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="sticky top-0 z-40 w-full bg-background/80 backdrop-blur">
       <div className="container flex h-16 items-center justify-between space-x-0">
         <MainNav />
         <MobileNav />
